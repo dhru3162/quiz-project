1d378c84166c7d697724a3053117424e144c8e5f
vaseemsarvaiya <vaseemsarvaiya786@gmail.com>
2024-03-10 15:26:32 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import HeroSectionPage from "@/src/Components/HeroSection/HeroSectionPage";
 import style from "@/src/Components/HeroSection/HeroSection.module.css"
+import Footer from "@/src/Components/Footer/FooterPage";
 export default function Home() {
   return (
     <div className={`${style.image}`}>
       <HeroSectionPage/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer/FooterCOntainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FooterCOntainer = () => {
+  return (
+    <div>FooterCOntainer</div>
+  )
+}
+
+export default FooterCOntainer
