58be2098408027c1b5f967c586fa09ac90fd5d5c
vaseemsarvaiya <vaseemsarvaiya786@gmail.com>
2024-03-26 15:53:33 +0530

    last upadate

--- src/Components/Dashboard/DashboardPage.tsx
+++ src/Components/Dashboard/DashboardPage.tsx
@@ -1,26 +1,34 @@
-// DashboardPage.jsx
 
-import React, { useState } from 'react';
+import { FaPlus } from "react-icons/fa";
 import Navbar from "../Navbar/Navbar"
+import ButtonTheme from "../Theme/Button/ButtonTheme";
 import Style from './Dashboard.module.scss'
 import DashboardModal from "./DashboardModal";
+import { Pane } from "evergreen-ui";
+import { BiEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 import DashboardCardsSkeleton from "../Loaders/Skeletons/DashboardCardsSkeleton";
 import Loader from "../Loaders/Loader";
 import AddQuizModal from "./AddQuizModel";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { BASE_API } from "@/src/lib/const";
-import { BiEdit } from 'react-icons/bi';
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
 
-const DashboardPage = (props:any) => {
+const DashboardPage = (props: any) => {
     const {
         quizData,
         role,
         isLoading,
+        goToQuizLoader,
         setGoToQuizLoader,
-    } = props;
-const router=useRouter()
+    } = props
+    const router = useRouter()
+
+
+
+
+
     const [editQuizData, setEditQuizData] = useState(null);
 
     const handleEditClick = ({quizId}:{quizId:any}) => {
@@ -40,15 +48,34 @@ console.log(handleEditClick,"useee")
             console.error('Error fetching quiz data:', error);
         }
     };
-
-    const cardsForAdmin = quizData?.map((item:any, index:any) => {
+    const cardsForUser = quizData?.map((item: any, index: number) => {
+        return (
+            <div key={index} className='flex justify-center w-full'>
+                <div className={`max-md:w-[80%] xl:w-[90%] text-center ${Style.cardBg} w-full rounded-xl p-5 space-y-2`}>
+                    <div className={`${Style.cardTitle} text-2xl font-bold line-clamp-1`}>
+                        {item.title}
+                    </div>
+                    <div className={`line-clamp-2`}>
+                        {item.description}
+                    </div>
+                    <DashboardModal
+                        {...{
+                            item,
+                            setGoToQuizLoader,
+                        }}
+                    />
+                </div>
+            </div>
+        )
+    })
+    const cardsForAdmin = quizData?.map((item: any, index: number) => {
         return (
             <div key={index} className='flex justify-center w-full h-full'>
                 <div className={`max-md:w-[80%] xl:w-[90%] text-center ${Style.cardBg} w-full rounded-xl relative group`}>
     
                     <div className={`${Style.cardHoverAction} rounded-lg justify-center h-full opacity-0 group-hover:opacity-100 duration-500 absolute inset-x-0 flex items-center group-hover:translate-y-0 translate-y-4 transform`} >
                         <div className="inline-flex text-3xl space-x-3">
-                            {/* Handle Edit Click */}
+                            {/* Use Link component to navigate */}
                             <div onClick={() => handleEditClick(item.id)}>
                                 <BiEdit/>
                             </div>
@@ -71,6 +98,34 @@ console.log(handleEditClick,"useee")
     
     return (
         <>
+            {role === 'user' &&
+                <>
+                    {goToQuizLoader && <Loader bgBlur />}
+                    <Navbar />
+                    <div className='bgColor min-h-screen flex justify-center'>
+                        <div className="max-w-screen-2xl w-full p-10">
+
+                            <div className="flex justify-center mt-10 max-md:mt-7">
+                                <div className='text-center'>
+                                    <h1 className={`${Style.headText} text-5xl md:text-4xl font-bold max-md:text-3xl`}>
+                                        Welcome to QuizWiz
+                                    </h1>
+                                    <p className={`${Style.headText} mt-3 max-md:mt-1 max-md:text-sm`}>
+                                        Select topic below to start your quiz
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className='mt-10 gap-8 h-fit grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+                                {isLoading && <DashboardCardsSkeleton />}
+                                {!isLoading && cardsForUser}
+                            </div>
+
+                        </div>
+                    </div>
+                </>
+            }
+
             {role === 'admin' &&
                 <>
                     <Navbar />
@@ -100,4 +155,4 @@ console.log(handleEditClick,"useee")
     )
 }
 
-export default DashboardPage;
+export default DashboardPage
