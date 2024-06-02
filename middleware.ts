70c433630b23b52c306503bfcaebfd82bd7ea958
Dhru Viramgama <dhru@linearloop.io>
2024-06-02 17:03:26 +0530

    middleware add

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
-import toast from 'react-hot-toast';
 
 export const middleware = (request: NextRequest) => {
     const path = request.nextUrl.pathname;
 
-    const isPublicPath = path === '/login' || path === '/' || path === '/forgot-password';
+    const isPublicPath = path === '/login' || path === '/' || path === '/forgot-password' || path.startsWith('/forgot-password');
     const commanPrivatePath = path === '/dashboard' || path === '/profile' || path === "/change-password";
     const isPrivateUsersPath = path === '/history' || path === "/playquiz";
     const isPrivateAdminPath = path === '/users' || path === "/editquiz";
