17b7c518fa40cea6d8bd3627daba60021fa3010b
Dhru Viramgama <dhru@linearloop.io>
2025-03-23 11:45:49 +0530

    Add gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,30 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <meta name="Dhru Viramgama" content="This is amazing web app for playing quiz" />
+      <Head>
+        {/* <!-- Google Tag Manager --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WPG4GB3X');
+            `,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
+        <meta name="Dhru Viramgama" content="This is amazing web app for playing quiz" />
+      </Head>
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WPG4GB3X"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Main />
         <NextScript />
       </body>
